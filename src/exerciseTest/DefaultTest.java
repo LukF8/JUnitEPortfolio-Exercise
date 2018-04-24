2234bbd49b82609ffedb1d9979a6ebfbf2ee4d17
LukasFeltes <l.feltes97@web.de>
2018-04-24 16:26:35 +0200

    implemented Default Test

---
+++ src/exerciseTest/DefaultTest.java
@@ -0,0 +1,13 @@
+package exerciseTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DefaultTest {
+
+	@Test
+	public void Test() {
+		fail("Not yet implemented");
+	}
+}
